8d65a59f42b7e2d42f527d9da87cc0da325aa36a
manis404 <manis404@gmail.com>
2023-04-18 00:05:47 +0100

    first commit

--- markdown-html2.py
+++ markdown-html2.py
@@ -6,9 +6,11 @@ import markdown, natsort, os, json, pathlib
 
 REPO_PATH = pathlib.Path(os.environ['GITHUB_WORKSPACE'])
 INPUT_LIST = json.loads(os.environ['INPUT_INPUT_FILES'])
-BUILTIN_STYLESHEET : str = ""
-EXTENSIONS : list = ["pymdownx.extra"]
-EXTENSION_CONFIGS : dict = json.loads({})
+OUTPUT_LIST = json.loads(os.environ['INPUT_OUTPUT_FILES'])
+EXCLUDE_DUPLICATES : bool = json.loads(os.environ['INPUT_EXCLUDE_DUPLICATES'])
+BUILTIN_STYLESHEET : str = os.environ['INPUT_BUILTIN_STYLESHEET']
+EXTENSIONS : list = json.loads(os.environ['INPUT_EXTENSIONS'])
+EXTENSION_CONFIGS : dict = json.loads(os.environ['INPUT_EXTENSION_CONFIGS'])
 
 md = markdown.Markdown(extensions=EXTENSIONS, extension_configs=EXTENSION_CONFIGS, output_format="html5")
 
@@ -25,23 +27,18 @@ else:
     style = ""
 
 for input_sublist in INPUT_LIST:
+    md.reset()
+    md_str = ""
     
     for input_path_glob_str in input_sublist:
         input_path_list = natsort.natsorted([str(p) for p in REPO_PATH.glob(input_path_glob_str)])
         for input_path_str in input_path_list:
                 
-                md.reset()
-                md_str = ""
-
                 with open(input_path_str, 'r') as input_file:
                     md_str = input_file.read() + "\n"
 
-                    fileName = os.path.basename(input_path_str)[:-3]
-
-                    output_path = REPO_PATH + "/markdown/" + fileName + ".html"
-                    
-                    print("Generating", output_path)
-
+                    print("Generating", input_path_str + "/../markdown/")
+                    output_path = input_path_str + "/../markdown"
                     html = "<!DOCTYPE html>\n" + style + "\n" + md.convert(md_str)
                     with open(output_path, 'w') as output_file:
                         output_file.write(html)
---
+++ test2.py
@@ -0,0 +1,2 @@
+name = 'abcd.md'
+print(name[:-3])
